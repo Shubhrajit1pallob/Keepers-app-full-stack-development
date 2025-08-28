c71f7afa98a25d98940b2d61b04d920a5ef2aaa4
Shubhrajit1pallob <spallob@asu.edu>
2025-08-28 14:32:46 -0700

    Updated the config.py file in for the backend.

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,9 +4,15 @@ class Config:
     # Change from:
     # SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/keepers_db'
     
+    db_user = os.getenv('DB_USER', 'postgres')
+    db_password = os.getenv('DB_PASSWORD', 'mypass')
+    db_host = os.getenv('DB_HOST', 'localhost')
+    db_port = os.getenv('DB_PORT', '5431')
+    db_name = os.getenv('DB_NAME', 'keepers_db')
+
     # To (add +psycopg):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg://postgres:mypass@localhost:5431/keepers_db'
-    
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
