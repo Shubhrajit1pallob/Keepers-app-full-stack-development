f4d258968437695656b64c478efa6e417ea1c41a
Shubhrajit1pallob <spallob@asu.edu>
2025-08-28 17:05:11 -0700

    Created the deployment workflow file.

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -6,3 +6,12 @@ output "api_endpoint" {
   value       = "http://${aws_instance.this.public_ip}:8080"
   description = "The API endpoint for the application"
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
+
+output "instance_elastic_ip" {
+  value = aws_eip.this.public_ip
+  description = "The Elastic IP address of the instance"
+}
