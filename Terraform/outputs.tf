6bb8d3274d2f84130d09e8b41d91a6d5171e46ab
Shubhrajit1pallob <spallob@asu.edu>
2025-09-07 19:07:04 -0700

    Made changes to the outpus file in Terraform/

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -15,3 +15,18 @@ output "instance_elastic_ip" {
   value       = aws_eip.this.public_ip
   description = "The Elastic IP address of the instance"
 }
+
+output "rds_instance_endpoint" {
+  value = module.database.rds_instance_endpoint
+  description = "The RDS instance endpoint"
+}
+
+output "rds_instance_port" {
+  value = module.database.rds_instance_port
+  description = "The RDS instance port"
+}
+
+output "rds_instance_name" {
+  value = module.database.rds_instance_name
+  description = "The RDS instance name"
+}
